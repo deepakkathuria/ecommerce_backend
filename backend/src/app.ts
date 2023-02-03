a258f476586755e7b015dc5bf1e509776f716f45
Elie Bissor <eliebiss.26@gmail.com>
2023-02-03 12:53:18 +0100

    Remove port condition

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,9 +15,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 export {};
