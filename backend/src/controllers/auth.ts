9477b53ecc50719e439a85d7633ff2c8e192061a
Elie Bissor <eliebiss.26@gmail.com>
2023-02-03 15:14:57 +0100

    Rever last commit.

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -5,36 +5,34 @@ const jsonwebtoken = require("jsonwebtoken");
 export const signUp = async (req: any, res: any) => {
   const { name, email, password } = req.body;
 
-  if (name && email && password) {
-    bcrypt.hash(password, 10).then((hash: any) => {
-      sql.query(
-        `select * from users where email="${email}"`,
-        (err: any, rows: any) => {
-          if (err) throw err;
-          if (rows.length < 1) {
-            sql.query(
-              `INSERT INTO users (name, email, password)
+  bcrypt.hash(password, 10).then((hash: any) => {
+    sql.query(
+      `select * from users where email="${email}"`,
+      (err: any, rows: any) => {
+        if (err) throw err;
+        if (rows.length < 1) {
+          sql.query(
+            `INSERT INTO users (name, email, password)
                 VALUES (
                     "${name}",
                     "${email}",
                     "${hash}"
                 )`,
-              (err: any, rows: any) => {
-                if (err) throw err;
-                res.json({
-                  status: 201,
-                  rows,
-                  message: "Your account has been succesfully created !",
-                });
-              }
-            );
-          } else {
-            res.json({ status: 400, message: "This email already exist." });
-          }
+            (err: any, rows: any) => {
+              if (err) throw err;
+              res.json({
+                status: 201,
+                rows,
+                message: "Your account has been succesfully created !",
+              });
+            }
+          );
+        } else {
+          res.json({ status: 400, message: "This email already exist." });
         }
-      );
-    });
-  }
+      }
+    );
+  });
 };
 
 export const signIn = (req: any, res: any) => {
