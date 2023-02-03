979e4f90b007c0c166fac0448af70e051a17292c
Elie Bissor <eliebiss.26@gmail.com>
2023-02-03 12:50:04 +0100

    Removed host from app.listen

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,6 +16,8 @@ app.use(express.json());
 app.use(routes);
 
 const port = process.env.PORT || 3001;
-app.listen(port, "0.0.0.0");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 export {};
